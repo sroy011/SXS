f8881f597d89cb270a739ca4b023f8a3e7dc65f5
Sujoy Roy <78295357+sroy011@users.noreply.github.com>
2022-12-26 20:44:02 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,21 +1,5 @@
 print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
-import os, platform
 
-try:
 
-        import requests
-
-except:
-
-        os.system('pip2 install requests')
-
-import requests
-
-bit = platform.architecture()[0]
-
-if bit == "64bit":
-
-        from RUN import Subscraption
-
-        Subscraption()        
+import Subscraption()
 
